2f41a27b2ccee96b8fae79bd7461dd38c6205e48
sandro brunner <brunnersandro@hotmail.com>
2014-08-08 10:54:28 +0200

    create apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: apache
+# Recipe:: default
+#
+# Copyright 2014, ICCLAB
+#
+# All rights not reserved - Do Redistribute
+#
+
+# chef-repo/cookbooks/apache/recipes/default.rb
+
+# Steps that have to be completed (Part of Demo Webcast)
+# 1. Install Apache webserver
+# 2. Start the apache service
+# 3. Make sure that the service starts on reboot
+# 4. Write out home page
+
+
+
