59be08803d0af8f84ab2f6536666c1615f111dcb
sandro brunner <brunnersandro@hotmail.com>
2014-08-08 16:26:34 +0200

    added apt cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,5 +15,15 @@
 # 3. Make sure that the service starts on reboot
 # 4. Write out home page
 
+package "httpd" do 
+	action :install 
+end
 
+service "httpd" do
+	action [:start, :enable]
+end
 
+cookbook_file "/var/www/html/index.html" do
+	source "index.html"
+	mode "0644"
+end
