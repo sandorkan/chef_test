6cedcdf9ed2d93b1804c43d026d6e608339f9e0b
sandro brunner <brunnersandro@hotmail.com>
2014-08-12 14:57:54 +0200

    added template to create dynamic html file

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -15,15 +15,30 @@
 # 3. Make sure that the service starts on reboot
 # 4. Write out home page
 
-package "httpd" do 
+package_name = "apache2"
+service_name = "apache2"
+document_root = "/var/www"
+
+if node["platform"] == "centos"
+  package_name = "httpd"
+  service_name = "httpd"
+  document_root = "/var/www/html"
+end
+
+package package_name do
 	action :install 
 end
 
-service "httpd" do
+service service_name do
 	action [:start, :enable]
 end
 
-cookbook_file "/var/www/html/index.html" do
-	source "index.html"
-	mode "0644"
+template "#{document_root}/index.html" do
+  source "index.html.erb"
+  mode "0644"
 end
+
+# cookbook_file "#{document_root}/index.html" do
+#   source "index.html"
+#   mode "0644"
+# end
